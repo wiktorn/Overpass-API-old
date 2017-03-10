933a70d7a0871685b4cb4a0d26bb90e31dd7707b
W <github@vink.pl>
2017-03-10 22:35:03 +0100

    add entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -eo pipefail
+shopt -s nullglob
+OVERPASS_META=${OVERPASS_META:-no}
+OVERPASS_MODE=${OVERPASS_MODE:-clone}
+
+if [ -ne /db/db ] ; then
+    if [ "$OVERPASS_MODE" = "clone" ]; then
+        shift
+        /app/bin/download_clone.sh --db-dir=/db/db --source=http://dev.overpass-api.de/api_drolbr/ "--meta=$OVERPASS_META"
+    fi
+
+    if [ "$OVERPASS_MODE" = "init" ]; then
+        wget -O /db/planet "$OVERPASS_PLANET_URL"
+        /app/bin/init_osm3s.sh /db/planet /db/db /app/bin "--meta=$OVERPASS_META"
+        rm /db/planet
+    fi
+fi
+
+/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
