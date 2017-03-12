15d18869f96a8f212b2f27d019b97c2c455361db
W <github@vink.pl>
2017-03-12 20:38:54 +0100

    .

--- src/bin/apply_osc_to_db.sh
+++ src/bin/apply_osc_to_db.sh
@@ -31,7 +31,7 @@ if [[ ! ${EXEC_DIR:0:1} == "/" ]]; then
 
 DB_DIR=`$EXEC_DIR/dispatcher --show-dir`
 
-if [ ! -d $DB_DIR ] ; then
+if [[ ! -d $DB_DIR ]] ; then
     echo "Can't find DB_DIR. Returned value: $DB_DIR"
     exit 1;
 fi
